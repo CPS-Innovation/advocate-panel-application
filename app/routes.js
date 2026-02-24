ba2262045ad93eb3f3f03698463e86b707ce07c1
Estela Gaspar <54668579+that-firefly@users.noreply.github.com>
2026-02-24 00:35:31 +0000

    new pages

--- app/routes.js
+++ app/routes.js
@@ -47,6 +47,27 @@ router.post('/panel-answer', function (req, res) {
 })
 
 
+router.post('/solicitor-panel-answer', function (req, res) {
+
+  // Read form value (Prototype Kit stores it in req.session.data)
+  const answer = req.session.data['panel']
+
+  // Redirect depending on the value
+  if (answer === 'crownCourtGeneralCrime') {
+    res.redirect('./about-you-questions/solicitor-crown-court-levels.html')
+  } else if (answer === 'rapeSeriousSexualOffences') {
+    res.redirect('./about-you-questions/solicitor-crown-court-levels.html')
+  } else if (answer === 'specialist') {
+    res.redirect('./about-you-questions/solicitor-crown-court-levels.html')
+  } else if (answer === 'other') {
+    res.redirect('./about-you-questions/solicitor-crown-court-levels.html')
+  } else {
+    // If nothing selected, redirect back or show an error page
+    res.redirect('/#')
+  }
+})
+
+
 // General Crime //
 // 3. Route to handle "Select the a level" answer
 router.post('/levels-answer', function (req, res) {
@@ -70,6 +91,31 @@ router.post('/levels-answer', function (req, res) {
 })
 
 
+router.post('/solicitor-levels-answer', function (req, res) {
+
+  // Read form value (Prototype Kit stores it in req.session.data)
+  const answer = req.session.data['levels']
+
+  // Redirect depending on the value
+  if (answer === 'level1') {
+    res.redirect('./about-you-questions/solicitor-new-joiner-level-1.html')
+  } else if (answer === 'level2') {
+    res.redirect('./about-you-questions/solicitor-new-joiner-level-2-4.html')
+  } else if (answer === 'level3') {
+    res.redirect('./about-you-questions/solicitor-new-joiner-level-2-4.html')
+  } else if (answer === 'level4') {
+    res.redirect('./about-you-questions/solicitor-new-joiner-level-2-4.html')
+  } else {
+    // If nothing selected, redirect back or show an error page
+    res.redirect('/#')
+  }
+})
+
+
+
+
+
+
 // 4. Route to handle "Select a membership type" answer
 router.post('/membership-type-answer', function (req, res) {
 
@@ -89,7 +135,40 @@ router.post('/membership-type-answer', function (req, res) {
 
 
 
+router.post('/solicitor-membership-type-answer', function (req, res) {
 
+  // Read form value (Prototype Kit stores it in req.session.data)
+  const answer = req.session.data['membership']
+
+  // Redirect depending on the value
+  if (answer === 'permanent') {
+    res.redirect('./about-you-questions/solicitor-level-2-4-permanent.html')
+  } else if (answer === 'temporary') {
+    res.redirect('./about-you-questions/solicitor-level-2-4-temporary.html')
+  } else {
+    // If nothing selected, redirect back or show an error page
+    res.redirect('/#')
+  }
+})
+
+
+
+// 5. Route to handle "What type of solicitor are you?" answer
+router.post('/typeSolicitor-answers', function (req, res) {
+
+  // Read form value (Prototype Kit stores it in req.session.data)
+  const answer = req.session.data['typeSolicitor']
+
+  // Redirect depending on the value
+  if (answer === 'crownCourt') {
+    res.redirect('./about-you-questions/solicitor-question-court-panel.html')
+  } else if (answer === 'youthCourt') {
+    res.redirect('./about-you-questions/solicitor-wishing-to-practice.html')
+  } else {
+    // If nothing selected, redirect back or show an error page
+    res.redirect('/#')
+  }
+})
 
 
 
