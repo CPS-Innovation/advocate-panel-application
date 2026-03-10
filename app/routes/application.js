c146e0f4b7b1f55be1e29c12edc23da79451aa2c
uxtom-cps <thomas.brown@cps.gov.uk>
2026-03-10 16:00:48 +0000

    First Merge of Role | Personal Details task list | Panel & Level  (#38)
    * Added app/routes-old.js
    
    * Added
    app/routes/app.js
    POST route for /account
    Added relative path to top of templates and Nunjucks form components to
    app/views/account.html
    app/views/register.html
    app/views/login.html
    
    * Added POST routes for
    question-what-describes-you
    register + made form components Nunjucks + top line relative path
    
    * Added Nunjucks form componenst + route for
    views/current-chambers/employment-type.html
    Removed space from folder name app/views/personal details
    
    * Tweaked route for /create-account
    
    * Created dummy CYA page to bridge from personal details task list to about-you-questions/question-court-panel
    
    * Added profile page /views/profile.html +  GET and POST routes to routes/app.js
    
    * Changes to nunjucks components on
    about-you-questions/question-court-panel.html
    about-you-questions/crown-court-levels.html
    
    * Add routes and level 4 crown court question pages
    
    * Added
    about-you-questions/check-answers.html
    Plugged in radios and conditional templatinbg for Level 1/4 London Secondee
    
    * Changed button text on app/views/profile.html to 'Add panel information'
    
    * Added routing for sign in / Sign out loop + template for signg-out.html
    Plus app/views/_components/header Macro
    
    * Removed productName from main govuk header on /_includes/header.njk
    
    * Added loop for app/views/examples-work/examples-work-advocacy.html with status update on task list
    
    * Added POST route for /personal-details/personal-details
    coditinal tag logic to top of views/task-list.html
    Fixed field input name to barNumber
    
    * Added views/_includes/task-status.njk --> to update statuses in task-list.html
    
    * Added Completed status loop --> Preferred circuits and crown courts section in task-list.html
    
    * All equalities flow added except check your answers. Returs rto taslk-lisy.html --> status updates to Completed
    
    * Updated views/check.html to accomodate the new register flow screens
    
    * Updated views/check.html to accomodate the new register flow screens with added h3 headers
    
    ---------
    
    Co-authored-by: UxTom <thomas.h.brown@icloud.com>

---
+++ app/routes/application.js
@@ -0,0 +1,60 @@
+/////////// app/routes/application.js
+
+module.exports = router => {
+
+   ////////// COURT PANEL
+  router.post('/about-you-questions/question-court-panel', (req, res) => {
+    const courtPanel = req.session.data['courtPanel']
+
+    if (courtPanel === 'Crown Court General Crime') {
+      res.redirect('/about-you-questions/crown-court-levels')
+    } else if (courtPanel === 'Rape and serious sexual offences') {
+      res.redirect('/about-you-questions/question-court-panel')
+    } else if (courtPanel === 'Specialist') {
+      res.redirect('/about-you-questions/question-court-panel')
+    } else {
+      res.redirect('/about-you-questions/question-court-panel')
+    }
+  })
+
+  ////////// CROWN COURT LEVELS
+  router.post('/about-you-questions/crown-court-levels', (req, res) => {
+    const crownCourtLevel = req.session.data['crownCourtLevel']
+
+    if (crownCourtLevel === 'Level 1') {
+      res.redirect('/about-you-questions/new-joiner-level-1')
+    } else if (crownCourtLevel === 'Level 4') {
+      res.redirect('/about-you-questions/london-secondee')
+    } else {
+      res.redirect('/about-you-questions/london-secondee')
+    }
+  })
+
+  ////////// NEW JOINER LEVEL 1
+  router.post('/about-you-questions/new-joiner-level-1', (req, res) => {
+    res.redirect('/about-you-questions/check-answers')
+  })
+
+  ////////// LONDON SECONDEE (LEVEL 4)
+  router.post('/about-you-questions/london-secondee', (req, res) => {
+    res.redirect('/about-you-questions/check-answers')
+  })
+
+  ////////// EXAMPLES OF WORK - ADVOCACY
+  router.post('/examples-work/examples-work-advocacy', (req, res) => {
+    const completed = req.session.data['haveYouCompletedThisSection']
+    const advocacy = req.session.data['advocacyExample']
+
+    if (completed === 'yes') {
+      req.session.data['advocacyStatus'] = 'completed'
+    } else if (advocacy && advocacy.trim() !== '') {
+      req.session.data['advocacyStatus'] = 'inProgress'
+    } else {
+      req.session.data['advocacyStatus'] = 'notStarted'
+    }
+
+    res.redirect('/start-application')
+  })
+  
+
+}
---
+++ app/routes/personal-details.js
@@ -0,0 +1,169 @@
+/////////// app/routes/app.js
+
+const { route } = require("../routes-old")
+
+module.exports = router => {
+
+  ////////// START PAGE
+  router.post('/account', (req, res) => {
+    const hasAccount = req.session.data['new']['hasAccount']
+    
+    if (hasAccount === 'yes') {
+      res.redirect('/login')
+    } else {
+      res.redirect('/create-account')
+    }
+  })
+
+  ////////// CHECK - PERSONAL DETAILS
+  router.post('/check', (req, res) => {
+    res.redirect('/profile')
+  })
+
+  ////////// LOGIN
+  router.get('/login', (req, res) => {
+    res.render('login')
+  })
+
+  router.post('/login', (req, res) => {
+    req.session.data.user = {}
+    res.redirect('/profile')
+  })
+
+  ////////// SIGN OUT
+  router.get('/sign-out', (req, res) => {
+    req.session.destroy()
+    res.redirect('/sign-out-confirmation')
+  })
+
+  router.get('/sign-out-confirmation', (req, res) => {
+    res.render('sign-out')
+  })
+
+  ////////// REGISTER 
+  router.post('/create-account', (req, res) => {
+    res.redirect('/question-what-describes-you')
+  })
+
+  ////////// ROLE 
+  router.post('/question-what-describes-you', (req, res) => {
+    const role = req.session.data['yourRoleIs']
+
+    if (role === 'Barrister') {
+      res.redirect('/task-list')
+    } else if (role === 'Solicitor') {
+      res.redirect('/question-what-describes-you')
+    } else {
+      res.redirect('/question-what-describes-you')
+    }
+  })
+
+  ////////// PERSONAL DETAILS - NAME AND BAR NUMBER
+  router.post('/personal-details/personal-details', (req, res) => {
+    req.session.data['personalDetailsStatus'] = 'completed'
+    const returnUrl = req.query.returnUrl
+    res.redirect(returnUrl || '/task-list')
+  })
+
+  ////////// EMPLOYMENT TYPE 
+  router.post('/current-chambers/employment-type', (req, res) => {
+    const employmentType = req.session.data['employmentType']
+    const returnUrl = req.query.returnUrl
+
+    if (employmentType === 'memberChambers') {
+      res.redirect(returnUrl || '/current-chambers/name-chambers')
+    } else if (employmentType === 'soleTrader') {
+      res.redirect(returnUrl || '/current-chambers/sole-trader-adding-address')
+    } else if (employmentType === 'solicitorsFirm') {
+      res.redirect(returnUrl || '/current-chambers/solicitor-firm-adding-address')
+    } else {
+      res.redirect('/current-chambers/employment-type')
+    }  
+  })
+
+  ////////// NAME OF CHAMBERS
+  router.post('/current-chambers/name-chambers', (req, res) => {
+    req.session.data['whereYouWorkStatus'] = 'completed'
+    const returnUrl = req.query.returnUrl
+    res.redirect(returnUrl || '/task-list')
+  })
+
+  ////////// PREFERRED CIRCUIT
+  router.post('/preferred-circuit/circuit-question-page', (req, res) => {
+    const returnUrl = req.query.returnUrl
+    res.redirect(returnUrl || '/preferred-circuit/crown-courts-circuit/crown-courts-select-page')
+  })
+
+  ////////// CROWN COURTS SELECT
+  router.post('/preferred-circuit/crown-courts-circuit/crown-courts-select-page', (req, res) => {
+    req.session.data['preferredCircuitStatus'] = 'completed'
+    const returnUrl = req.query.returnUrl
+    res.redirect(returnUrl || '/task-list')
+  })
+
+  ////////// CALL TO BAR AND PUPILAGE
+  router.post('/year-call-degree-qualifications/year-call', (req, res) => {
+    req.session.data['callToBarAndPupilage'] = 'completed'
+    const returnUrl = req.query.returnUrl
+    res.redirect(returnUrl || '/task-list')
+  })
+
+  ////////// DEGREE AND POSTGRAD QUALS
+  router.post('/year-call-degree-qualifications/degree-qualifications', (req, res) => {
+    req.session.data['degreeAndPostGradQualifications'] = 'completed'
+    const returnUrl = req.query.returnUrl
+    res.redirect(returnUrl || '/task-list')
+  })
+
+////////// EQUALITIES MONITORING
+  router.post('/equalities/equalities-questions-1', (req, res) => {
+    res.redirect('/equalities/disability-question-2')
+  })
+
+  ////////// DISABILITY
+  router.post('/equalities/disability-question-2', (req, res) => {
+    res.redirect('/equalities/socio-economic-background-3')
+  })
+
+  ////////// SOCIO ECONOMIC
+  router.post('/equalities/socio-economic-background-3', (req, res) => {
+    res.redirect('/equalities/occupation-questions-4')
+  })
+
+  ////////// PARENTS QUALS
+  router.post('/equalities/occupation-questions-4', (req, res) => {
+    res.redirect('/equalities/occupation-parents-5')
+  })
+
+  ////////// PARENTS OCCUPATION
+  router.post('/equalities/occupation-parents-5', (req, res) => {
+    res.redirect('/equalities/occupation-parents-6')
+  })
+
+  ////////// HIGHEST INCOME IN HOUSEHOLD
+  router.post('/equalities/occupation-parents-6', (req, res) => {
+    res.redirect('/equalities/occupation-parents-7')
+  })
+
+  ////////// HOW MANY EMPLOYED IN PARENTS COMPANY
+  router.post('/equalities/occupation-parents-7', (req, res) => {
+    res.redirect('/equalities/occupation-parents-8')
+  })
+
+  ////////// HIGHEST INCOME SUPERVISOR
+  router.post('/equalities/occupation-parents-8', (req, res) => {
+    res.redirect('/equalities/school-meals-9')
+  })
+
+  ////////// FREE SCHOOL MEALS
+  router.post('/equalities/school-meals-9', (req, res) => {
+    res.redirect('/equalities/assessment-10')
+  })
+
+  ////////// SELF ASSESSMENT
+  router.post('/equalities/assessment-10', (req, res) => {
+    req.session.data['equalityInformation'] = 'completed'
+    res.redirect('/task-list')
+  })
+
+}
