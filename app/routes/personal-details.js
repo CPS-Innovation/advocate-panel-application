7565884be3d56d4e6a97f82cd6928aa1db21181e
UxTom <thomas.h.brown@icloud.com>
2026-03-10 10:33:29 +0000

    Added views/_includes/task-status.njk --> to update statuses in task-list.html

--- app/routes/personal-details.js
+++ app/routes/personal-details.js
@@ -81,42 +81,11 @@ module.exports = router => {
     }  
   })
 
-  ////////// COURT PANEL
-  router.post('/about-you-questions/question-court-panel', (req, res) => {
-    const courtPanel = req.session.data['courtPanel']
-
-    if (courtPanel === 'Crown Court General Crime') {
-      res.redirect('/about-you-questions/crown-court-levels')
-    } else if (courtPanel === 'Rape and serious sexual offences') {
-      res.redirect('/about-you-questions/question-court-panel')
-    } else if (courtPanel === 'Specialist') {
-      res.redirect('/about-you-questions/question-court-panel')
-    } else {
-      res.redirect('/about-you-questions/question-court-panel')
-    }
+  ////////// NAME OF CHAMBERS
+  router.post('/current-chambers/name-chambers', (req, res) => {
+    req.session.data['whereYouWorkStatus'] = 'completed'
+    res.redirect('/task-list')
   })
 
-  ////////// CROWN COURT LEVELS
-  router.post('/about-you-questions/crown-court-levels', (req, res) => {
-    const crownCourtLevel = req.session.data['crownCourtLevel']
-
-    if (crownCourtLevel === 'Level 1') {
-      res.redirect('/about-you-questions/new-joiner-level-1')
-    } else if (crownCourtLevel === 'Level 4') {
-      res.redirect('/about-you-questions/london-secondee')
-    } else {
-      res.redirect('/about-you-questions/london-secondee')
-    }
-  })
-
-  ////////// NEW JOINER LEVEL 1
-  router.post('/about-you-questions/new-joiner-level-1', (req, res) => {
-    res.redirect('/about-you-questions/check-answers')
-  })
-
-  ////////// LONDON SECONDEE (LEVEL 4)
-  router.post('/about-you-questions/london-secondee', (req, res) => {
-    res.redirect('/about-you-questions/check-answers')
-  })
 
 }
